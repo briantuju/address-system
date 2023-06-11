7b0e64beda369750823b32e8016ce65074d9b3a0
Brian Tuju <briantuju@gmail.com>
2023-06-11 15:46:15 +0300

    Configure address database structure

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Address;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AddressFactory extends Factory
+{
+    protected $model = Address::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->word,
+            'slug' => $this->faker->unique()->slug,
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Address;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        DB::table('addresses')->truncate();
+        Address::factory(10)->create();
     }
 }
