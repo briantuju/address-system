7f806006ef09434e9f2d21550c1890dc6290a810
Brian Tuju <briantuju@gmail.com>
2023-06-11 15:05:59 +0300

    Setup app layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,3 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ config('app.name') ?? 'Address Management System' }}</title>
-</head>
-<body>
-
-</body>
-</html>
+<x-layout>
+    <x-slot:title>{{'Home | '. config('app.name')  }}</x-slot:title>
+</x-layout>
