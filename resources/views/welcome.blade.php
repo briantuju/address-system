0959c94ee31cf4dba21a2725d8efc962fb727e81
Brian Tuju <briantuju@gmail.com>
2023-06-11 15:11:59 +0300

    Setup tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
     <x-slot:title>{{'Home | '. config('app.name')  }}</x-slot:title>
+
+    <h1 class="text-3xl font-bold text-amber-700">
+        {{ config('app.name') }}
+    </h1>
 </x-layout>
