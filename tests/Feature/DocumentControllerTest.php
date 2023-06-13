88e300a3e1452241ed932e62ec9534fc70790e81
Brian Tuju <briantuju@gmail.com>
2023-06-13 08:05:25 +0300

    Test document controller store method

---
+++ tests/Feature/DocumentControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Address;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+
+class DocumentControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_the_store_method()
+    {
+        Storage::fake('documents');
+
+        $address = Address::factory()->create();
+        $file = UploadedFile::fake()->image('doc.jpg')->size(150);
+
+        $this->post(route('address.document.store', $address), [
+            'file' => $file
+        ])->assertRedirect();
+
+        $this->assertDatabaseHas('documents', [
+            'name' => $file->hashName(),
+        ]);
+    }
+}
