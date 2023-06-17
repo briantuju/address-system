48f8d17a4d35c1d9a73b5d63b70ce69eee423fb2
Brian Tuju <briantuju@gmail.com>
2023-06-17 22:15:32 +0300

    Update auth/guest layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,14 +39,14 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex items-center justify-between mt-8">
+            <x-primary-button class="mr-4">
+                {{ __('Register') }}
+            </x-primary-button>
+
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
         </div>
     </form>
 </x-guest-layout>
